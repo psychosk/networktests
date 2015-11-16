4f8a6bb6eb2bed53c9ba9ed6e51eae7ac544e94d
psychosk <siddharth.kilam@gmail.com>
2015-11-16 12:43:57 +0530

    a

--- networktests/networktests/UDPClient.java
+++ networktests/networktests/UDPClient.java
@@ -9,12 +9,12 @@ public class UDPClient {
 		String url = args[0];
 		int port = Integer.parseInt(args[1]);
 		
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
 		DatagramSocket clientSocket = new DatagramSocket();
 		InetAddress IPAddress = InetAddress.getByName("localhost");
 		byte[] sendData = new byte[1024];
 		byte[] receiveData = new byte[1024];
-		String sentence = inFromUser.readLine();
+		String sentence = "smartsense";
 		sendData = sentence.getBytes();
 		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
 		
