516b25219393af7e640d1cc6c4122f2b8e0f8e4f
psychosk <siddharth.kilam@gmail.com>
2015-11-16 14:10:24 +0530

    a

--- networktests/networktests/TCPServer.java
+++ networktests/networktests/TCPServer.java
@@ -5,22 +5,23 @@ import java.io.*;
 import java.net.*;
 
 class TCPServer {
-   public static void main(String args[]) {
-	   int port = Integer.parseInt(args[0]);
-      String data = "Toobie ornaught toobie";
-      try {
-         ServerSocket srvr = new ServerSocket(port);
-         Socket skt = srvr.accept();
-         System.out.print("Server has connected!\n");
-         PrintWriter out = new PrintWriter(skt.getOutputStream(), true);
-         System.out.print("Sending string: '" + data + "'\n");
-         out.print(data);
-         out.close();
-         skt.close();
-         srvr.close();
-      }
-      catch(Exception e) {
-         System.out.print("Whoops! It didn't work!\n");
-      }
-   }
+	public static void main(String args[]) {
+		int port = Integer.parseInt(args[0]);
+		String data = "Toobie ornaught toobie";
+		try {
+			ServerSocket srvr = new ServerSocket(port);
+			while (true) {
+				Socket skt = srvr.accept();
+				System.out.print("Server has connected!\n");
+				PrintWriter out = new PrintWriter(skt.getOutputStream(), true);
+				System.out.print("Sending string: '" + data + "'\n");
+				out.print(data);
+				out.close();
+				skt.close();
+			}			
+			//srvr.close();
+		} catch (Exception e) {
+			System.out.print("Whoops! It didn't work!\n");
+		}
+	}
 }
